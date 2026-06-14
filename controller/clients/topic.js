312d0cb8a5be70ce8103655db1c947ecbb610a41
Cabap8126 <cabap8126@gmail.com>
2026-06-14 17:00:49 +0700

    Ngô Trọng Mạnh database Topic

---
+++ controller/clients/topic.js
@@ -0,0 +1,39 @@
+const Topic = require('../model/topic');
+module.exports.create  = async (req, res) => {
+    const formbody =  req.body
+    if(fomrbody){
+        const topic = new Topic({formbody})
+        await topic.save()
+        res.redirect("/clients/topic")
+    }
+    else{
+        console.log("không nhận được dữ liệu")
+    }
+}
+module.exports.read = async (req, res) => {
+    const topics = await Topic.find({
+        status: "active"
+    })
+    console.log(topics)
+}
+module.exports.update = async (req, res) => {
+    const id = req.params.id
+    const formbody = req.body
+    if(formbody && id){
+        await Topic.updateOne({_id: id}, formbody)
+        console.log("đã cập nhật")
+    }
+    else{
+        console.log("không nhận đc data")
+    }
+}
+modulexx.exports.delete = async (req, res) => {
+    const id = req.params.id
+    if(id){
+        await Topic.updateOne({_id: id}, {status: "inactive"})
+        console.log("đã xóa")
+    }
+    else{
+        console.log("không nhận đc id")
+    }
+}
