e356f1b44e512d107469dba7680827d7836028bb
Trần Đình Mạnh <24100139@st.phenikaa-uni.edu.vn>
2026-06-14 11:03:41 +0000

    part 2 cho song.js

--- controller/clients/song.js
+++ controller/clients/song.js
@@ -51,8 +51,3 @@ module.exports = ( app ) => {
 const express = require('express');
 const router = express();
 const controller = require("../../controller/clients/song")
-router.get("/",controller.read)
-router.post("/",controller.create)
-router.put("/:id",controller.update)
-router.delete("/:id",controller.delete)
-module.exports = router
