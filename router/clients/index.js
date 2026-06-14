1086ec73c5d055fe4f371a5001ff6a6f4ffef78f
Trần Đình Mạnh <24100139@st.phenikaa-uni.edu.vn>
2026-06-14 10:34:32 +0000

    viết crud cho song

--- router/clients/index.js
+++ router/clients/index.js
@@ -1,6 +1,6 @@
 const songRouter = require("./song")
 const singerRouter = require("./singer")
 module.exports = ( app ) => {
-    app.use("/",songRouter)
+    app.use("/clients/song", songRouter)
     app.use("/clients/singer", singerRouter)
 }
